0d4c2f6b0bf3711982c060c84b01da052da81f82
Kostiantyn Koriahin <koka4ka428@gmail.com>
2023-02-01 20:54:41 +0200

    feature: phoneNumberTransformer has been updated

--- src/shared/utils/properties.transformers.spec.ts
+++ src/shared/utils/properties.transformers.spec.ts
@@ -4,13 +4,22 @@ import { phoneNumberTransformer } from './properties.transformers';
 
 describe('Properties Transformers utils services', () => {
   describe('phoneNumberTransformer util service', () => {
-    it('should return a transformed phone number', () => {
-      const expectedResult = '+38 (044) 123-45-67';
-      const actualResult = phoneNumberTransformer({
-        value: '+380441234567',
-      } as TransformFnParams);
+    const dataSet = [
+      { input: '+380441234567', expectedOutput: '+38 (044) 123-45-67' },
+      { input: '+380961234567', expectedOutput: '+38 (096) 123-45-67' },
+      { input: '+38044321456', expectedOutput: '+38044321456' },
+      { input: '0443214567', expectedOutput: '044321456' },
+    ];
 
-      expect(actualResult).toMatch(expectedResult);
-    });
+    it.each(dataSet)(
+      'should return a transformed phone number',
+      ({ input, expectedOutput }) => {
+        const actualResult = phoneNumberTransformer({
+          value: input,
+        } as TransformFnParams);
+
+        expect(actualResult).toMatch(expectedOutput);
+      },
+    );
   });
 });
